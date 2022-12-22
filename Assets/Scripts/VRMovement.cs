1acfeec7fa9b0bd612d595287f3b808d740b7141
shrmanator <dovindustries@gmail.com>
2022-12-21 20:00:30 -0500

    Forward and backward movement works now

--- Assets/Scripts/VRMovement.cs
+++ Assets/Scripts/VRMovement.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 using System.IO;
+using Oculus.VR;
 
 
 /*
@@ -16,50 +17,43 @@ public class VRMovement : MonoBehaviour
 {
     public GameObject Head;
     public GameObject LeftHand, RightHand;
-    GameObject hmd;
     private List<XRNodeState> mNodeStates = new List<XRNodeState>();
     private Vector3 mHeadPos, mLeftHandPos, mRightHandPos;
     private Vector3 mHeadVelocity, mLeftHandVelocity, mRightHandVeocity, mHeadAngularVelocity;
     private Quaternion mHeadRot, mLeftHandRot, mRightHandRot;
 
-    Quaternion headRotation;
-
-    private float minWalkingVelocity = 0.23f;
     private Vector3 cameraVelocity = Vector3.zero;
-    
     private Vector3 yVectorDirection;
-    private Vector3 crossProduct; // represents the direction the player moves towards
+    private Vector3 crossProduct; // the direction the player moves towards
 
     [SerializeField] float playerSpeed = 2.0f; // increasing this value increases the movement speed
     [SerializeField] float maxForwardAngle = 110.0f;  // the maximum angle the hmd points in relative to the forward walking motion. Once this value is exceeded, the camera moves backwards.
     [SerializeField] Vector3 currDirection;
-    [SerializeField] float smoothTime = 0.3F;
+    [SerializeField] float smoothTime = 0.03f;
+    [SerializeField] float minWalkingVelocity = 0.23f;
 
     StreamWriter writer; // for debugging
     int time; // for debugging
 
-
     public OVRPose trackerPose = OVRPose.identity;
     
-	void Awake()
+	private void Awake()
 	{
 		OVRCameraRig rig = GameObject.FindObjectOfType<OVRCameraRig>();
-
 		if (rig != null)
-			rig.UpdatedAnchors += OnUpdatedAnchors;
+			rig.UpdatedAnchors += OnUpdateOrientAnchors;
 	}
 
-	void OnUpdatedAnchors(OVRCameraRig rig)
+	private void OnUpdateOrientAnchors(OVRCameraRig rig)
 	{
-		if (!enabled)
-			return;
-
-        // problem is somehwere around here
-		OVRPose pose = rig.trackerAnchor.ToOVRPose(true).Inverse();
-		pose = trackerPose * pose;
-		rig.trackingSpace.FromOVRPose(pose, true);
+		if (enabled)
+        {
+            OVRPose pose = new OVRPose();
+            // pose.position = new Vector3(1, 2, 3);
+            pose.orientation = Quaternion.Euler(0, 127.9f, 0);
+            rig.trackingSpace.FromOVRPose(pose, true);
+        }
 	}
-
 
     private void Start()
     {
@@ -85,7 +79,6 @@ public class VRMovement : MonoBehaviour
             switch (nodeState.nodeType)
             {
                 case XRNode.Head:
-                    // DO ALL HMD LOGIC HERE
                     nodeState.TryGetVelocity(out mHeadVelocity);
                     nodeState.TryGetAngularVelocity(out mHeadAngularVelocity);
                     nodeState.TryGetPosition(out mHeadPos);
@@ -100,37 +93,39 @@ public class VRMovement : MonoBehaviour
                     float positionY = mHeadPos.y;
                     float positionZ = mHeadPos.z;
 
-                    // print(Camera.main.transform.position + " x, z " + positionZ);
-
-                    if (getVelocityX() > 0) 
+                    if (velocityX > 0) 
                     {
                         // right
                         yVectorDirection = Vector3.up;
                     }
-                    else if (getVelocityX() < 0)
+                    else if (velocityX < 0)
                     {
                         // left
                         yVectorDirection = -Vector3.up;
                     }
 
-                    // Calculate the direction the player should move
+                    // Calculate the direction the player should move towards
                     currDirection = Vector3.Cross(yVectorDirection, mHeadVelocity).normalized;
 
-                    if ((getVelocityX() >= minWalkingVelocity || getVelocityX() <= -minWalkingVelocity || getVelocityZ() >= minWalkingVelocity || getVelocityZ() <= -minWalkingVelocity))
+                    if ((velocityX >= minWalkingVelocity || velocityX <= -minWalkingVelocity || velocityZ >= minWalkingVelocity || velocityZ <= -minWalkingVelocity))
                     {
-                        Debug.DrawRay(yVectorDirection, -currDirection*50, Color.red);  // horizontal velocity
-                        Debug.DrawRay(Vector3.up, Camera.main.transform.forward*50, Color.green); // hmd forward direction
-                        Debug.DrawLine(-currDirection, Camera.main.transform.forward, Color.yellow); // angle between both rays
-
-                        Head.transform.position += currDirection * playerSpeed * Time.deltaTime;
-
                         if (Vector3.Angle(Camera.main.transform.forward, -currDirection) <= maxForwardAngle) 
                         {
-                            Head.transform.Translate(-currDirection * playerSpeed * Time.deltaTime);
+                            Debug.DrawRay(yVectorDirection, -currDirection*50, Color.red);  // horizontal velocity
+                            Debug.DrawRay(yVectorDirection, Camera.main.transform.forward*50, Color.green); // direction the hmd is facing
+                            Debug.DrawLine(-currDirection, Camera.main.transform.forward, Color.yellow); // angle between both rays
+                            
+                            Vector3 targetPosition = Head.transform.position + -currDirection * playerSpeed * Time.deltaTime;
+                            Head.transform.position = Vector3.SmoothDamp(Head.transform.position, targetPosition, ref cameraVelocity, smoothTime);
                         }
                         else 
                         {
-                            Head.transform.Translate(currDirection * playerSpeed * Time.deltaTime);
+                            Debug.DrawRay(yVectorDirection, currDirection*50, Color.red);  // horizontal velocity
+                            Debug.DrawRay(yVectorDirection, Camera.main.transform.forward*50, Color.green); // direction the hmd is facing
+                            Debug.DrawLine(currDirection, Camera.main.transform.forward, Color.yellow); // angle between both rays
+                            
+                            Vector3 targetPosition = Head.transform.position + currDirection * playerSpeed * Time.deltaTime;
+                            Head.transform.position = Vector3.SmoothDamp(Head.transform.position, targetPosition, ref cameraVelocity, smoothTime);
                         }
                     }
                 break;
@@ -139,89 +134,9 @@ public class VRMovement : MonoBehaviour
         time += 1;
     }
 
+ 
     /*
-    Returns the current headset's velocity.
-    */
-    private Vector3 getHMDVelocity()
-    {
-        return mHeadVelocity;
-    }
-
-    /*
-    Returns XRNode's rotation for X axis (roll)
-    */
-    private float getRotationX()
-    {
-        return mHeadRot[0];
-    }
-
-    /*
-    Returns XRNode's rotation for Z axis (pitch)
-    */
-    private float getRotationZ()
-    {
-        return mHeadRot[2];
-    }
-
-    /*
-    Returns XRNode's rotation for Y axis (yaw)
-    */
-    private float getRotationY()
-    {
-        return mHeadRot[1];
-    }
-
-    /*
-    Returns XRNode's position for X axis (left, right)
-    */
-    private float getPositionX()
-    {
-        return mHeadPos[0];
-    }
-
-    /*
-    Returns XRNode's position for Y axis (up, down)
-    */
-    private float getPositionY()
-    {
-        return mHeadPos[1];
-    }
-
-    /*
-    Returns XRNode's position for Z axis (forward, backward)
-    */
-    private float getPositionZ()
-    {
-        return mHeadPos[2];
-    }
-
-    /*
-    Returns XRNode's velocity for X axis (left, right)
-    */
-    private float getVelocityX()
-    {
-        return mHeadVelocity[0];
-    }
-
-
-    /*
-    Returns XRNode's velocity for Y axis (up, down)
-    */
-    private float getVelocityY()
-    {
-        return mHeadVelocity[1];
-    }
-
-    /*
-    Returns XRNode's velocity for X axis (forward, backward)
-    */
-    private float getVelocityZ()
-    {
-        return mHeadVelocity[2];
-    }
-
-    /*
-    Just closes the writing stream (for testing only)
+    Just closes the writing stream (for testing)
     */
     private void OnApplicationQuit()
     {
